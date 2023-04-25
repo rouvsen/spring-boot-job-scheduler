1794648eb7401d86bb16f71aa41f849b37f7018c
rovshan <rouvsen@gmail.com>
2023-04-25 18:36:38 +0400

    User Dao added

---
+++ src/main/java/com/rouvsen/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.rouvsen.dao;
+
+import com.rouvsen.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao
+        extends JpaRepository<User, Long> {
+}
