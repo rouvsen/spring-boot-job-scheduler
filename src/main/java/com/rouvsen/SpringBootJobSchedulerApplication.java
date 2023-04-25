508ae03c2f9c98540a8814af77aaac99b19ca847
rovshan <rouvsen@gmail.com>
2023-04-25 18:36:23 +0400

    initial commit

---
+++ src/main/java/com/rouvsen/SpringBootJobSchedulerApplication.java
@@ -0,0 +1,15 @@
+package com.rouvsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SpringBootJobSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJobSchedulerApplication.class, args);
+    }
+
+}
